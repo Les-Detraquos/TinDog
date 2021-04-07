a61e49e5e5686c09f63285c41ff8209713702312
Jonas <jonas.laplace@esme.fr>
2021-04-07 12:11:13 +0200

    First version committed

---
+++ app/src/main/java/com/example/tindog/UserScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tindog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_screen)
+    }
+}
