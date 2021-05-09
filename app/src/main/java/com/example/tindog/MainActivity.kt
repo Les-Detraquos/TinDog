6785a37fb6865c918e317b8b5369609400a76156
Jonas <jonas.laplace@esme.fr>
2021-05-09 13:00:07 +0200

    6th commit : authentification done!

--- app/src/main/java/com/example/tindog/MainActivity.kt
+++ app/src/main/java/com/example/tindog/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.core.view.View
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -21,67 +23,30 @@ class MainActivity : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // [START initialize_auth]
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-        // [END initialize_auth]
+        setContentView(R.layout.activity_main)
 
-        setContentView(R.layout.activity_main);
-
-        val button = findViewById<Button>(R.id.button_login)
-        button.setOnClickListener {
-            val intent =  Intent(this, SlideScreenActivity::class.java)
-
-            startActivity(intent)
-        }
-
-        val button2 = findViewById<TextView>(R.id.signUp)
-        button2.setOnClickListener {
-            val intent =  Intent(this, SignUpActivity::class.java)
-
-            startActivity(intent)
-        }
+        auth = FirebaseAuth.getInstance()
     }
 
-    // [START on_start_check_user]
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            reload();
-        }
-    }
-    // [END on_start_check_user]
+    fun login(view: android.view.View){
+        val getEmail = findViewById<EditText>(R.id.email_textField)
+        val getPassword = findViewById<EditText>(R.id.password_textField)
+        val email = getEmail.text.toString()
+        val password = getPassword.text.toString()
 
-    private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intent = Intent(this,SlideScreenActivity::class.java)
+                startActivity(intent)
+                finish()
             }
-        // [END sign_in_with_email]
-    }
-    private fun updateUI(user: FirebaseUser?) {
-
-    }
-
-    private fun reload() {
-
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG)
+        }
     }
 
-    companion object {
-        private const val TAG = "EmailPassword"
+    fun goToRegister(view: android.view.View){
+        val intent = Intent(this,SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/tindog/SignUpActivity.kt
+++ app/src/main/java/com/example/tindog/SignUpActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.core.view.View
 import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
@@ -20,53 +22,31 @@ class SignUpActivity : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // [START initialize_auth]
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-        // [END initialize_auth]
-
         setContentView(R.layout.activity_sign_up)
 
-        val button = findViewById<Button>(R.id.button_saveAccount)
-        button.setOnClickListener {
-            val intent = Intent(this, SlideScreenActivity::class.java)
-
-            startActivity(intent)
-        }
+        auth = FirebaseAuth.getInstance()
     }
 
-    private fun createAccount(email: String, password: String) {
-        // [START create_user_with_email]
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                }
-                else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    updateUI(null)
-                }
+    fun register(view: android.view.View){
+        val getEmail = findViewById<EditText>(R.id.email_textField)
+        val getPassword = findViewById<EditText>(R.id.password_textField)
+        val email = getEmail.text.toString()
+        val password = getPassword.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                val intent = Intent(this,SlideScreenActivity::class.java)
+                startActivity(intent)
+                finish()
             }
-        // [END create_user_with_email]
-
-    }
-    private fun updateUI(user: FirebaseUser?) {
-
-    }
-
-    private fun reload() {
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext, exception.localizedMessage,Toast.LENGTH_LONG).show()
+        }
 
     }
 
-    companion object {
-        private const val TAG = "EmailPassword"
+    fun goToLogin(view: android.view.View){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
