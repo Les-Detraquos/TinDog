ed6350f5347b145eb653fd3f110461a7813d51fc
Jonas <jonas.laplace@esme.fr>
2021-05-15 15:26:18 +0200

    89th commit : creation of a cloud on Firebase to store the animals's pictures, but some problems to resolve to finish it completely

--- app/src/main/java/com/example/tindog/SignUpActivity.kt
+++ app/src/main/java/com/example/tindog/SignUpActivity.kt
@@ -3,16 +3,20 @@ package com.example.tindog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 
 //ask for help about resolving these two reference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.referencecode.database.models.User
+import com.google.firebase.storage.ktx.storage
+
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -20,15 +24,23 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     // [END declare_auth]
 
+    // [START storage_field_declaration]
+    lateinit var storage: FirebaseStorage
+    // [END storage_field_declaration]
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_sign_up)
 
         auth = FirebaseAuth.getInstance()
+
+        // [START storage_field_initialization]
+        storage = Firebase.storage
+        // [END storage_field_initialization]
     }
 
-    fun register(view: android.view.View){
+    fun register(view: View){
         val getEmail = findViewById<EditText>(R.id.email_textField)
         val getPassword = findViewById<EditText>(R.id.password_textField)
         val getPhoneNumber = findViewById<EditText>(R.id.phoneNumber_textField)
@@ -53,7 +65,7 @@ class SignUpActivity : AppCompatActivity() {
 
     }
 
-    fun goToLogin(view: android.view.View){
+    fun goToLogin(view: View){
         val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
     }
@@ -71,4 +83,23 @@ class SignUpActivity : AppCompatActivity() {
         val user = User(name, surname, phoneNumber, email, password)
         database.child("users").child(userId).setValue(user)
     }
+
+    fun downloadAnimalPictures() {
+
+        // Create a reference to a file from a Google Cloud Storage URI
+        val cat1Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/cat1.jfif")
+        val cat2Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/cat2.jfif")
+        val cat3Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/cat3.jfif")
+        val cat4Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/cat4.jfif")
+        val dog1Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/dog1.jfif")
+        val dog2Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/dog2.jfif")
+        val dog3Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/dog3.jfif")
+        val dog4Ref = storage.getReferenceFromUrl("gs://tindog-d82b1.appspot.com/dog4.jfif")
+
+        //je crois que ca n'a aucun sens de les dl depuis le cloud alors que je les ai deja sur l'ordi
+        //Il faut voir si il y a un moyen d'acceder directement au cloud et les display dans le recycler
+        //view sans les telecharger en local
+    }
+
+
 }
