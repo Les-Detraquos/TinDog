88ac6a222e49eedd3e1346eb7a4f30d884b7b92e
StanTheDev <stan.granger2@gmail.com>
2021-05-18 16:54:57 +0200

    first commit shesh

---
+++ app/src/main/java/com/example/tindog/AdapterRclVwSwipe.kt
@@ -0,0 +1,38 @@
+package com.example.tindog
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class AdapterRclVwSwipe (private val ListRclVwSwipe: List<RclVwSwipe>) : RecyclerView.Adapter<AdapterRclVwOptions.ViewHolderSwipe>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderOptions {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.rclvw1,
+            parent, false)
+
+        return ViewHolderOptions(itemView)
+    }
+
+
+
+    override fun getItemCount() = ListRclVwOptions.size
+
+
+    override fun onBindViewHolder(holder: ViewHolderOptions, position: Int) {
+        val currentItem = ListRclVwOptions[position]
+
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView.text = currentItem.text1
+    }
+
+    class ViewHolderOptions(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView : ImageView = itemView.findViewById(R.id.image_view)
+        val textView : TextView = itemView.findViewById(R.id.text_view)
+
+    }
+}
